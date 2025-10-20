46306fb2452f70291baac27fc2fa18a2df291ba8
Polina Vasilyeva <Polinova31@gmail.com>
2025-10-20 14:28:38 +0300

    Add files via upload

---
+++ datacenter/getting_duration.py
@@ -0,0 +1,27 @@
+from django.utils import timezone
+import datetime
+
+def is_visit_long(visit, minutes=60):
+	if not visit.leaved_at:
+		return False
+	duration = visit.leaved_at - visit.entered_at
+	return duration > datetime.timedelta(minutes=minutes)
+
+
+def get_duration(visit):
+	if visit.leaved_at:
+		return visit.leaved_at - visit.entered_at
+	else:
+		return timezone.now() - visit.entered_at
+
+
+def format_duration(duration):
+	total_seconds = duration.total_seconds()
+	hours = int(duration.total_seconds() // 3600)
+	minutes = int((duration.total_seconds() % 3600) // 60)
+	return f"{hours}ч {minutes}мин"
+
+
+def get_formatted_duration(visit):
+	duration = get_duration(visit)
+	return format_duration(duration)
