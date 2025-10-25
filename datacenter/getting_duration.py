0ea9329829f7568fe29a175498929e3b4d2de84a
Polina Vasilyeva <Polinova31@gmail.com>
2025-10-25 20:27:13 +0300

    Исправила названия переменных

--- datacenter/getting_duration.py
+++ datacenter/getting_duration.py
@@ -16,11 +16,11 @@ def get_duration(visit):
 
 
 def format_duration(duration):
-	total_seconds = duration.total_seconds()
-	hours_in_storage = int(duration.total_seconds() // 3600)
-	minutes_in_storage = int((duration.total_seconds() % 3600) // 60)
+	seconds_in_hour = 3600
+	minutes_in_hour = 60
+	hours_in_storage = int(duration.total_seconds() // seconds_in_hour)
+	minutes_in_storage = int((duration.total_seconds() % seconds_in_hour) // minutes_in_hour)
 	return f"{hours_in_storage}ч {minutes_in_storage}мин"
-
 
 def get_formatted_duration(visit):
 	duration = get_duration(visit)
